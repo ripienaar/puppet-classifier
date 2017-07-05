f1b1f5e0c56d4469e08adc9d53d28004daddad57
Thomas Mueller <thomas@chaschperli.ch>
2017-07-05 12:28:01 +0200

    Remove data_bindings and use module hiera data
    Makes it compatible with Puppet 4.10.5 and Puppet 5.0.1 because
    of [PUP-7751](https://tickets.puppetlabs.com/browse/PUP-7751)

--- lib/puppet/bindings/classifier/default.rb
+++
@@ -1,7 +0,0 @@
-Puppet::Bindings.newbindings('classifier::default') do
-  bind {
-    name "classifier"
-    in_multibind "puppet::path_based_data_provider_factories"
-    to_instance "Puppet::DataProviders::ClassifierDataProviderFactory"
-  }
-end
